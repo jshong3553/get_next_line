1e722c803cbb8e87f5a3ca255923804b8ad13134
Jin Hong <jinhong@e1z2r5p30.42.us.org>
2019-05-21 20:38:01 -0700

    get_next_line

--- get_next_line.c
+++ get_next_line.c
@@ -6,38 +6,69 @@
 /*   By: jinhong <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/15 13:16:55 by jinhong           #+#    #+#             */
-/*   Updated: 2019/05/15 13:49:17 by jinhong          ###   ########.fr       */
+/*   Updated: 2019/05/20 19:06:41 by jinhong          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft/libft.h"
 #include "get_next_line.h"
 
-int	get_next_line(const int fd, char **line)
+int		copy(int fd, char **line, char **s, int i)
 {
-	static char	*c[2147483647];
-	char		buffer[BUFF_SIZE + 1];
-	char		*tmp;
-	ssize_t		b;
-	int			endl;
+	char	*temp;
 
-	if (fd < 0 || (!c[fd] && !(c[fd] = ft_strnew(1))) || !line)
-		return (-1);
-	while (!ft_strchr(c[fd], '\n') && (b = read(fd, buffer, BUFF_SIZE)) > 0)
+	if (s[fd][i] == '\0')
 	{
-		buffer[b] = '\0';
-		tmp = c[fd];
-		c[fd] = ft_strjoin(c[fd], buffer);
-		ft_strdel(&tmp);
+		*line = ft_strdup(s[fd]);
+		ft_strdel(&s[fd]);
 	}
-	if (b == -1 || !*(tmp = c[fd]))
-		return (b == -1 ? -1 : 0);
-	if ((endl = (ft_strchr(c[fd], '\n') > 0)))
-		*line = ft_strsub(c[fd], 0, ft_strchr(c[fd], '\n') - c[fd]);
-	else
-		*line = ft_strdup(c[fd]);
-	c[fd] = ft_strsub(c[fd], (unsigned int)(ft_strlen(*line) + endl),
-			(size_t)(ft_strlen(c[fd]) - (ft_strlen(*line) + endl)));
-	ft_strdel(&tmp);
-	return (!(!c[fd] && !ft_strlen(*line)));
+	else if (s[fd][i] == '\n')
+	{
+		*line = ft_strsub(s[fd], 0, i);
+		temp = ft_strdup(&s[fd][i + 1]);
+		free(s[fd]);
+		s[fd] = temp;
+		if (s[fd][0] == '\0')
+			ft_strdel(&s[fd]);
+	}
+	return (1);
+}
+
+int		check_line(int fd, char **line, char **s)
+{
+	int		i;
+
+	i = 0;
+	while (s[fd][i] != '\n' && s[fd][i] != '\0')
+		i++;
+	copy(fd, line, s, i);
+	return (1);
+}
+
+int		get_next_line(const int fd, char **line)
+{
+	static char		*s[255];
+	char			buff[BUFF_SIZE + 1];
+	char			*temp;
+	int				r;
+
+	if (fd < 0 || line == NULL)
+		return (-1);
+	while ((r = read(fd, buff, BUFF_SIZE)) > 0)
+	{
+		buff[r] = '\0';
+		if (s[fd] == NULL)
+			s[fd] = ft_strnew(1);
+		temp = ft_strjoin(s[fd], buff);
+		free(s[fd]);
+		s[fd] = temp;
+		if (ft_strchr(buff, '\n'))
+			break ;
+	}
+	if (r == 0 && (s[fd] == NULL || s[fd][0] == '\0'))
+		return (0);
+	if (r < 0)
+		return (-1);
+	check_line(fd, line, s);
+	return (1);
 }
--- get_next_line.h
+++ get_next_line.h
@@ -6,13 +6,13 @@
 /*   By: jinhong <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/15 13:17:02 by jinhong           #+#    #+#             */
-/*   Updated: 2019/05/15 13:49:43 by jinhong          ###   ########.fr       */
+/*   Updated: 2019/05/20 19:14:10 by jinhong          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
-# define BUFF_SIZE 32
+# define BUFF_SIZE 42
 # include <unistd.h>
 
 int					get_next_line(const int fd, char **line);
